2a964dea479da219a000841f8c4364238ee7cecb
Robert Kaye <rob@musicbrainz.org>
2019-12-30 22:25:25 +0000

    It can now roll in a stable manner, always keeping the distance it should. wow, very promising!

--- software/strings.py
+++ software/strings.py
@@ -5,6 +5,7 @@ from threading import Thread
 import pigpio
 import serial
 from encoder import decoder
+from simple_pid import PID
 
 BUTTON_0 = 17
 BUTTON_1 = 8
@@ -16,6 +17,7 @@ HALL_2_1 = 24
 DEVICE = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__Arduino_Uno_64932343938351313012-if00"
 WINDOW_SIZE = 5
 DEBOUNCE_TIME = .01
+SAMPLE_TIME = .075
 
 STATE_SETUP = 1
 STATE_SETUP_B0 = 2
@@ -58,15 +60,18 @@ def pig_callback(GPIO, level, tick):
     ts.pig_callback(GPIO, level, tick)
 
 
-class TrippyStrings(Thread):
+class TrippyStrings(object):
 
     def __init__(self):
-        Thread.__init__(self)
+        self.P = 1.0
+        self.I = .1
+        self.D = .05
+        self.pid = None
+        self.speed = 0
 
         self.pi = pigpio.pi()  
         self.encoder_0 = decoder(self.pi, HALL_1_0, HALL_1_1, callback_0)
         self.encoder_1 = decoder(self.pi, HALL_2_0, HALL_2_1, callback_1)
-        self.stop_thread = False
         self.button_0_state = False
         self.button_0_change = 0
         self.button_1_state = False
@@ -85,7 +90,7 @@ class TrippyStrings(Thread):
 
         try:
             self.ser = serial.Serial(DEVICE,
-                9600,
+                38400,
                 bytesize=serial.EIGHTBITS,
                 parity=serial.PARITY_NONE,
                 stopbits=serial.STOPBITS_ONE,
@@ -95,7 +100,6 @@ class TrippyStrings(Thread):
             sys.exit(-1)
 
         sleep(1)
-        self.start()
 
 
     def pig_callback(self, gpio, level, tick):
@@ -117,7 +121,8 @@ class TrippyStrings(Thread):
 
 
     def stop(self):
-        self.stop_thread = True
+        self.motor_speed(0, 0)
+        self.motor_speed(1, 0)
 
 
     def callback_0(self, way):
@@ -137,6 +142,7 @@ class TrippyStrings(Thread):
             speed = -speed
 
         if self.ser:
+            print(motor, speed)
             self.ser.write(bytes("%d%d\r\n" % (motor, speed), encoding="ascii"))
             
 
@@ -146,9 +152,14 @@ class TrippyStrings(Thread):
             self.pos_1 = 0
             self.motor_speed(0, 64)
             self.motor_speed(1, 64)
+            self.pid = PID(self.P, self.I, self.D, setpoint=0)
+            self.pid.sample_time = SAMPLE_TIME
+            self.speed = 64
+
         elif new_state == STATE_SETUP:
             self.motor_speed(0, 0)
             self.motor_speed(1, 0)
+            self.speed = 0
         elif new_state == STATE_SETUP_B0:
             self.motor_speed(0, 64)
         elif new_state == STATE_SETUP_B1:
@@ -166,7 +177,11 @@ class TrippyStrings(Thread):
 
     def run(self):
 
-        while not self.stop_thread:
+        i = 0
+        self.speed = 64
+
+        print("controller ready for action.")
+        while True:
 
             if not self.button_0_state and self.button_0_change and (time() - self.button_0_change > DEBOUNCE_TIME):
                 self.handle_event(EVENT_B0_HI)
@@ -188,7 +203,18 @@ class TrippyStrings(Thread):
                 self.button_1_change = 0
                 self.button_1_state = 0
 
-            sleep(.001)
+            if self.pid:
+                diff = self.pos_1 - self.pos_0
+                control = self.pid(self.pos_1 - self.pos_0)
+                pwm_diff = min(self.speed * 1.5, control)
+                mot_0 = int(self.speed - pwm_diff)
+                mot_1 = int(self.speed + pwm_diff)
+                self.motor_speed(0, mot_0)
+                self.motor_speed(1, mot_1)
+#                print("d: %d c: %.2f p: %d 0: %d 1: %d" % (diff, control, pwm_diff, mot_0, mot_1))
+
+            sleep(SAMPLE_TIME)
+            i += 1
 
         self.motor_speed(0, 0)
         self.motor_speed(1, 0)
@@ -199,13 +225,10 @@ def main():
 
     ts = TrippyStrings()
     ts.setup()
-
     try:
-        while True:
-            sleep(.1)
+        ts.run()
     except KeyboardInterrupt:
         ts.stop()
-        ts.join()
 
 
 if __name__ == "__main__":
